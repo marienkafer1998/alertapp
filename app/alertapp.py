cb90971373847e4d0b3d6f21ba2c7e0fb17f5e9f
Maria Malkova <marimalkova98@gmail.com>
2020-05-02 13:20:57 +0300

    receiver is added

--- app/alertapp.py
+++ app/alertapp.py
@@ -1,17 +1,115 @@
-from flask import Flask, request, jsonify
+#docker-compose up -d --build app
+
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+from collections import defaultdict
+from datetime import datetime
+# from models import Alerts, Incidents, TypeOfIncident, Users
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://alertapp:alertapp@db/alertapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://alertapp:alertapp@localhost/alertapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-@app.route('/receive', methods=['POST'])
+from enum import Enum
+
+class Alerts(db.Model):
+
+    __tablename__ = 'Alerts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    service = db.Column(db.String(64), unique=False, nullable=False)
+    severity = db.Column(db.String(64), nullable=False)
+    incident_id =  db.Column(db.Integer, db.ForeignKey('Incidents.id'), nullable=False)
+
+    # def __init__(self, service, severity):
+    #     self.service = service
+    #     self.severity = severity
+
+
+class TypeOfIncident(db.Model):
+
+    __tablename__ = 'TypeOfIncident'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    typeName = db.Column(db.String(30), unique=True, nullable=False)
+    description = db.Column(db.String(300), unique=True, nullable=False)
+    labels = db.Column(db.Text, nullable=False) #instead of JSON and ARRAY
+    incidents = db.relationship('Incidents', backref='TypeOfIncident', lazy=True)
+    # schedule_items = db.relationship('ScheduleItems', backref='TypeOfIncident', lazy=True)
+
+class Incidents(db.Model):
+
+    __tablename__ = 'Incidents'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    type_id =  db.Column(db.Integer, db.ForeignKey('TypeOfIncident.id'), nullable=False)
+    start = db.Column(db.DateTime, nullable=True)
+    end = db.Column(db.DateTime, nullable=True)
+    status = db.Column(db.String(30), nullable=False)
+    user_id =  db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=True)
+    alerts = db.relationship('Alerts', backref='Incidents', lazy=True)
+
+    # def __init__(self, service, severity):
+    
+    #     self.id = id
+    #     self.type_id = type_id
+    #     self.start = start
+    #     self.end = end
+    #     self.status = status
+
+
+class Users(db.Model):
+
+    __tablename__ = 'Users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(60), nullable=False)
+    fullName = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(260), nullable=False)
+    incidents = incidents = db.relationship('Incidents', backref='Users', lazy=True)
+
+
+
+@app.route('/receive', methods=['POST', 'GET'])
 def get_data():
     if request.method == 'POST':
         data = request.get_json()
+        num_types = db.session.query(TypeOfIncident).count()
+        num_alert = len(data)
+        types_incidents = db.session.query(TypeOfIncident.id, TypeOfIncident.labels).all()
+        print(num_types, types_incidents) 
 
-    return jsonify(data)
+        incidents = defaultdict(list)
+        # types_incidents = [(1, 'database'), (2, 'server error')]
+        print(num_alert) 
+        for alert in data:
+            labels = alert['labels'].values()
+            for type_ in types_incidents:
+                correct_type = True
+                for label in type_[1].split():
+                    if label not in labels:
+                        correct_type = False
+                        break
+                if correct_type:
+                    incidents[type_[0]].append(alert)
+        for incident in incidents.items():
+            incident_obj = Incidents(type_id = incident[0], start = datetime.now(), status = 'Active')
+            db.session.add(incident_obj)
+            db.session.commit()
+            for alert in incident[1]:
+                labels = alert['labels']
+                alert_obj = Alerts(severity=labels['severity'], service=labels['service'], Incidents=incident_obj)
+                db.session.add(alert_obj)
+                db.session.commit()
+        return jsonify(data)
+    return "no item"
+
+@app.route('/incidents', methods=['GET'])
+def show_incidents():
+    return render_template('incidents.html', incidents=Incidents.query.all())
+
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(host='0.0.0.0', port=1080)
+    app.run(host='0.0.0.0', port=1080, debug=True)
--- app/models.py
+++ app/models.py
@@ -1,18 +1,18 @@
 from alertapp import db
 from enum import Enum
 
-# class Alerts(db.Model):
+class Alerts(db.Model):
 
-#     __tablename__ = 'alerts'
+    __tablename__ = 'alerts'
 
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     alertname = db.Column(db.String(64), unique=False, nullable=False)
-#     resource = db.Column(db.String(64), unique=False, nullable=False)
-#     message = db.Column(db.Text, unique=False, nullable=False)
-#     hash_id = db.Column(db.String(64), unique=True, nullable=False)
-#     severity = db.Column(db.String(64), nullable=False)
-#     start = db.Column(db.DateTime, nullable=False)
-#     end = db.Column(db.DateTime, nullable=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    alertname = db.Column(db.String(64), unique=False, nullable=False)
+    resource = db.Column(db.String(64), unique=False, nullable=False)
+    message = db.Column(db.Text, unique=False, nullable=False)
+    severity = db.Column(db.String(64), nullable=False)
+    incident_id =  db.Column(db.Integer, db.ForeignKey('Incidents.id'), nullable=False)
+
+
 
 class TypeOfIncident(db.Model):
 
@@ -23,7 +23,7 @@ class TypeOfIncident(db.Model):
     description = db.Column(db.String(300), unique=True, nullable=False)
     labels = db.Column(db.Text, nullable=False) #instead of JSON and ARRAY
     incidents = db.relationship('Incidents', backref='TypeOfIncident', lazy=True)
-    schedule_items = db.relationship('ScheduleItems', backref='TypeOfIncident', lazy=True)
+    # schedule_items = db.relationship('ScheduleItems', backref='TypeOfIncident', lazy=True)
 
 class Incidents(db.Model):
 
@@ -35,6 +35,8 @@ class Incidents(db.Model):
     end = db.Column(db.DateTime, nullable=True)
     status = db.Column(db.String(30), nullable=False)
     user_id =  db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
+    alerts = db.relationship('Alerts', backref='Incidents', lazy=True)
+
 
 
 class Users(db.Model):
@@ -47,37 +49,37 @@ class Users(db.Model):
     password = db.Column(db.String(260), nullable=False)
     incidents = incidents = db.relationship('Incidents', backref='Users', lazy=True)
 
-class Channels(db.Model):
+# class Channels(db.Model):
 
-    __tablename__ = 'Channels'
+#     __tablename__ = 'Channels'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id =  db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
-    channel_source_type = db.Column(db.String(60), nullable=False) # probably should be in another table
-    channel_source_value = db.Column(db.String(150), nullable=False)
-
-
-class DayOfWeek(Enum):
-    monday = 1
-    tuesday = 2
-    wednesday = 3
-    thursday = 4
-    friday = 5
-    saturday = 6
-    sunday = 7
-
-class ScheduleItems(db.Model):
-
-    __tablename__ = 'ScheduleItems'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    type_id =  db.Column(db.Integer, db.ForeignKey('TypeOfIncident.id'), nullable=False)
-    # dayOfWeek = db.Column(db.Enum(DayOfWeek)) # probably doesn't work
-
-    duty_start = db.Column(db.DateTime, nullable=False)
-    duty_end = db.Column(db.DateTime, nullable=False)
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     user_id =  db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
+#     channel_source_type = db.Column(db.String(60), nullable=False) # probably should be in another table
+#     channel_source_value = db.Column(db.String(150), nullable=False)
 
 
-class ScheduleItemChannels(db.Model):
-    ScheduleItemId INTEGER REFERENCES ScheduleItem (Id),
-    ChannelId INTEGER REFERENCES Channels (Id)
+# class DayOfWeek(Enum):
+#     monday = 1
+#     tuesday = 2
+#     wednesday = 3
+#     thursday = 4
+#     friday = 5
+#     saturday = 6
+#     sunday = 7
+
+# class ScheduleItems(db.Model):
+
+#     __tablename__ = 'ScheduleItems'
+
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     type_id =  db.Column(db.Integer, db.ForeignKey('TypeOfIncident.id'), nullable=False)
+#     # dayOfWeek = db.Column(db.Enum(DayOfWeek)) # probably doesn't work
+
+#     duty_start = db.Column(db.DateTime, nullable=False)
+#     duty_end = db.Column(db.DateTime, nullable=False)
+
+
+# class ScheduleItemChannels(db.Model):
+#     ScheduleItemId INTEGER REFERENCES ScheduleItem (Id),
+#     ChannelId INTEGER REFERENCES Channels (Id)
