b75fb9055ef5d9a31218320bd95fd6747f2bacd6
Maria Malkova <marimalkova98@gmail.com>
2020-05-04 10:01:30 +0300

    function SendNotification added

--- app/alertapp.py
+++ app/alertapp.py
@@ -6,18 +6,20 @@ from collections import defaultdict
 from datetime import datetime
 from flask_login import login_user, login_required, logout_user, UserMixin,LoginManager, current_user
 from forms import TypeForm
-
+import time
+from itertools import cycle
 # from models import Alerts, Incidents, TypeOfIncident, Users
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://alertapp:alertapp@localhost/alertapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
 app.config.from_object('config')
 db = SQLAlchemy(app)
 manager = LoginManager(app)
 
-contact_list = 'ex@gmail.com, ex2@gmail.com'
-time = 1
+contact_list = 'ex@gmail.com ex2@gmail.com'
+
 
 class Alerts(db.Model):
 
@@ -40,6 +42,7 @@ class TypeOfIncident(db.Model):
     active = db.Column(db.Boolean, nullable=False)
     incidents = db.relationship('Incidents', backref='TypeOfIncident', lazy=True)
     schedule_items = db.relationship('ScheduleItems', backref='TypeOfIncident', lazy=True)
+
 
 class Incidents(db.Model):
 
@@ -123,6 +126,9 @@ def logout():
     logout_user()
     return redirect(url_for('login_page'))
 
+
+
+
 @app.route('/receive', methods=['POST', 'GET'])
 def get_data():
     if request.method == 'POST':
@@ -145,40 +151,75 @@ def get_data():
                 if correct_type:
                     incidents[type_[0]].append(alert)
         for incident in incidents.items():
+            print('[*] Создание инцидента')
             incident_obj = Incidents(type_id = incident[0], start = datetime.now(), status = 'Active')
+            print('[*] Добавляю инцидент')
             db.session.add(incident_obj)
+            print('[*] Делаю коммит')
             db.session.commit()
             for alert in incident[1]:
                 labels = alert['labels']
+                print('[*] Создаю алерт')
                 alert_obj = Alerts(severity=labels['severity'], service=labels['service'], Incidents=incident_obj)
+                print('[*] Добавлю алерт')
                 db.session.add(alert_obj)
+                print('[*] Коммит')
                 db.session.commit()
             # Создание NotificationRule для этого входящего инцидента
             day = datetime.today().isoweekday()
             sch = ScheduleItems.query.filter(ScheduleItems.dayOfWeek == day and ScheduleItems.type_id == incident_obj.type_id).first()
-            message = 'Incident!!!' #создать строку с описанием инцидента и ссылкой на него
-            notification = NotificationRules(incident_id = incident_obj.id, scheduleItem_id = sch.id, body = message, interval = time, status = True)
+            url_inc = 'http://localhost:1080/incidents/'+str(incident_obj.id)
+            message = 'Hurry! You have '+incident_obj.TypeOfIncident.typeName+' incidents! Check it here '+url_inc
+            notification = NotificationRules(incident_id = incident_obj.id, scheduleItem_id = sch.id, body = message, interval = 1, status = True)
             db.session.add(notification)
             db.session.commit()
+            print('[*] вхожу в функцию уведомления')
+            # send_notification(incident_obj)
+
+            send_notification(incident_obj, notification)
         return jsonify(data)
     return "no item"
 
-@app.route('/incidents', methods=['GET'])
+@app.route('/incidents', methods=['GET', 'POST'])
 # @login_required
 def show_incidents():
-    day = datetime.today().isoweekday()
-    print(day)
-    return render_template('incidents.html', incidents=Incidents.query.all())
+    incidents=Incidents.query.all()
+
+    return render_template('incidents.html', incidents=incidents) #ошибка тут!!!!
 
 
 @app.route('/types_incidents', methods=['GET'])
 # @login_required
 def show_types():
+    print('[*] Show types')
     on_types = TypeOfIncident.query.filter(TypeOfIncident.active == True).all()
     off_types = TypeOfIncident.query.filter(TypeOfIncident.active == False).all()
     return render_template('types.html', active=on_types, nonactive=off_types)
 
+def send_notification(incident, notification_rule):
+    print('[*] Внутри функции send_notification')
+    # print('[*] Обращение к бд за контактами')
+    contacts = ScheduleItems.query.filter(ScheduleItems.id == notification_rule.scheduleItem_id).first().channels
+    # print('[*] Контакты и айди', contacts, incident.id)
+    body = notification_rule.body
 
+    for _, contact in enumerate(cycle(contacts.split())):
+
+        print('[*] Sending to', contact)
+        # url=contact
+        # type = Channels.query.filter(Channels.channel_source_value == url).all().channel_source_type
+        # send_message(type, url, body)
+        
+        time.sleep(30)
+        # print('[*] Подключаемся к бд за новым статусом ')
+        incident = Incidents.query.filter(Incidents.id == incident.id).first()
+        db.session.commit()
+        print('[*] After conn to DB status is ', incident.status)
+        if incident.status!='Active':
+            print('Hurrah! You.ve done it! Incident status is ', incident.status)
+            break
+    print('the end')
+        
 
 
 @app.route('/schedule', methods=['GET'])
@@ -201,7 +242,7 @@ def create_type():
         else:
             active=False
         
-        type = TypeOfIncident(typeName=name, description=description, labels=labels, active=active, )
+        type = TypeOfIncident(typeName=name, description=description, labels=labels, active=active)
         db.session.add(type)
         db.session.commit()
 
@@ -220,7 +261,9 @@ def edit_type(id):
     if request.method == 'POST':
         form = TypeForm(formdata=request.form, obj=type)
         form.populate_obj(type)
+        
         db.session.commit()
+        db.session.flush()
         return redirect(url_for('show_types'))
     form = TypeForm(obj=type)
     return render_template('edit_type.html', type=type, form=form)
@@ -229,20 +272,32 @@ def edit_type(id):
 # @login_required
 def show_incident(id):
     incident = Incidents.query.get(id)
+    print('[*] Внутри конкретного инцидента')
+   
     option_list=['Active', 'Work-in progress', 'Postponed', 'Done']
     actual_status = incident.status
     option_list.remove(actual_status)
     option_list.append(actual_status)
     if request.method == 'POST':
+
+        print('[*] Changing incidents with id = ', incident.id)
+        print('[*] status before', incident.status)
         status = request.form.get('option')
         option_list.remove(status)
         option_list.append(status)
         incident.status=status
         if status=='Done':
             end = datetime.now()
+            print('[*] Добавляю время')  
+            notification = NotificationRules.query.filter(NotificationRules.incident_id == incident.id).first()
+            db.session.delete(notification)
+            db.session.commit
             incident.end = end
         if incident.user_id == None:
+            print('[*] Добавляю юзера')
             incident.user_id= current_user.id
+        print('[*] status after', incident.status)
+        print('[*] Делаю коммит ', incident.status)
         db.session.commit()   
 
         return render_template('incident.html', incident=incident, option_list=option_list)
